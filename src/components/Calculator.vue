37a68e887a95c84e14672301b333c68aa18ac7bf
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-13 14:32:10 +0200

    operator styles

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -9,22 +9,22 @@
     <div class="btn">AC</div>
     <div class="btn">+/-</div>
     <div class="btn">%</div>
-    <div class="btn">÷</div>
+    <div class="btn operator">÷</div>
     <div class="btn">7</div>
     <div class="btn">8</div>
     <div class="btn">9</div>
-    <div class="btn">x</div>
+    <div class="btn operator">x</div>
     <div class="btn">4</div>
     <div class="btn">5</div>
     <div class="btn">6</div>
-    <div class="btn">-</div>
+    <div class="btn operator">-</div>
     <div class="btn">1</div>
     <div class="btn">2</div>
     <div class="btn">3</div>
-    <div class="btn">+</div>
+    <div class="btn operator">+</div>
     <div class=" btn zero">0</div>
     <div class="btn">.</div>
-    <div class="btn">=</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
@@ -66,6 +66,11 @@ export default {
 .btn {
   background-color: #eee;
   border: 1px solid #333;
+}
+
+.operator {
+  background-color: rgb(71, 194, 173);
+  color: rgb(0, 17, 252);
 }
 
 </style>
