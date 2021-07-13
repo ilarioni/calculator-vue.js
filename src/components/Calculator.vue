e68827d22897af1f46f5125cdada29b195a98506
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-07-13 14:25:53 +0200

    style colors

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,26 +5,26 @@
   </div>
 
   <div class="calculator">
-    <div class="display"> Hey new Calculator </div>
-    <div>AC</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>÷</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>x</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
-    <div class="zero">0</div>
-    <div>.</div>
-    <div>=</div>
+    <div class="display"> <div class="number"> random number </div> </div>
+    <div class="btn">AC</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class=" btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
   </div>
 </template>
 
@@ -44,12 +44,28 @@ export default {
 .display {
   font-size: 40px;
   grid-column: 1 / 5;
-  background-color: blue;
-  color: white; 
+  background: linear-gradient( #f9d72f, #e0a82e);
+  /* #e0a82e */
+  color: rgb(235, 215, 215); 
+}
+
+.number {
+  width: 25%;
+  margin: 30px auto;
+  padding: 10px;
+  background: #000000;
+  align-content: center;
+  text-align: center;
+  border-radius: 20px;
 }
 
 .zero {
   grid-column: 1 / 3;
+}
+
+.btn {
+  background-color: #eee;
+  border: 1px solid #333;
 }
 
 </style>
